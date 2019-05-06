0d66332ea61435253c62790e8b2195a633191e5b
Xuefen <37746681+Sprinter1999@users.noreply.github.com>
2019-05-06 22:06:31 +0800

    Add files via upload

---
+++ keras/kerasTest.py
@@ -0,0 +1,17 @@
+#TODO:Use keras for the first time
+'''
+@Why jupyter? Julia Python R (JS Scala Go)
+@Julia is a new language mainly for Data science
+'''
+import keras
+import numpy as np
+import scipy as sp
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+import tensorflow as tf
+
+print("numpy ",np.__version__)
+print("tensorflow ",tf.__version__)
+print("keras ",keras.__version__)
+print("Let's dive into Deeep learning")
